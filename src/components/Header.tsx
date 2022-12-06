bf79bc370fc4bb6a14ce2b7b386efa3ba881897d
Thomas Bradley <tsbweb@gmail.com>
2022-12-06 11:02:38 +0000

    chakra-ui added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Flex, Heading } from '@chakra-ui/react';
 const Header = () => {
   return (
     <Flex padding="0.5rem" width="100%">
-      <Heading>Header</Heading>
+      <Heading>Heron Data</Heading>
     </Flex>
   );
 };
